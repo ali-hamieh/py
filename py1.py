eef92e2c4334be6f76cb22c6ba19d0f9827900d5
Ali Hamieh <hamieh8888@gmail.com>
2025-02-13 15:46:22 +0200

    number 3

--- py1.py
+++ py1.py
@@ -1,3 +1,6 @@
+
+# 1
+
 age = int(input("Please enter your age: "))
 if age > 10:
   
@@ -5,9 +8,20 @@ if age > 10:
 else: 
   print("the price is 5$")
 
+# 2
 
 number = int(input("Please enter a number: "))
 if (number%2) == 0:
   print("number is even")
 else: 
   print("number is odd")
+
+
+# number 3
+
+username = input("Please enter username ")
+password = int(input("Please enter password "))
+if (username == "admin" and password == 1234) :
+  print("access granted")
+else: 
+  print("access denied")
