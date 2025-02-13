70e984c4ab8199a12de97bc78d99d2970979d262
Ali Hamieh <hamieh8888@gmail.com>
2025-02-13 15:28:49 +0200

    check if number is divided by 2 and remainder is zero than it is even otherwise its odd

--- py1.py
+++ py1.py
@@ -1,6 +1,13 @@
 age = int(input("Please enter your age: "))
 if age > 10:
+  
   print("the price is 7$")
 else: 
   print("the price is 5$")
 
+
+number = int(input("Please enter a number: "))
+if (number%2) == 0:
+  print("number is even")
+else: 
+  print("number is odd")
